5b7f4d3b2d4889cb65965c64dd1288d1c5d28fd9
KG sensei <60828719+kgsensei@users.noreply.github.com>
2021-06-07 18:29:32 -0400

    Update site.js

--- js/site.js
+++ js/site.js
@@ -2,20 +2,20 @@
 //fetch("https://api.countapi.xyz/hit/kgsensei.dev/visits")
 console.log("Proudly built and published by kgsensei.\nCopyright (c) 2021 kgsensei.")
 
-window.addEventListener("DOMContentLoaded",function(){
-var scriptAppendOne=document.createElement("div")
-var scriptAppendTwo=document.createElement("div")
+window.addEventListener("load",function(){
+var scriptAppendOne=document.createElement("script")
+var scriptAppendTwo=document.createElement("script")
 var scriptAppendThr=document.createElement("script")
-scriptAppendOne.innerHTML="<script src=\"https://www.gstatic.com/firebasejs/8.6.5/firebase-app.js\"></script>"
-scriptAppendTwo.innerHTML="<script src=\"https://www.gstatic.com/firebasejs/8.6.5/firebase-analytics.js\"></script>"
+scriptAppendOne.src="https://www.gstatic.com/firebasejs/8.6.5/firebase-app.js"
+scriptAppendTwo.src="https://www.gstatic.com/firebasejs/8.6.5/firebase-analytics.js"
 scriptAppendThr.innerHTML="var firebaseConfig={\
-     apiKey:\"AIzaSyBfUW6Bl-5EPHIh_QiLsPaEg9FQMkJR7lM\",\
-     authDomain:\"kgsensei-957f7.firebaseapp.com\",\
-     projectId:\"kgsensei-957f7\",\
-     storageBucket:\"kgsensei-957f7.appspot.com\",\
-     messagingSenderId:\"816657691700\",\
-     appId:\"1:816657691700:web:1f5baa75f7c716cd9a8661\",\
-     measurementId:\"G-EW2T6G5DLH\"\
+apiKey:\"AIzaSyBfUW6Bl-5EPHIh_QiLsPaEg9FQMkJR7lM\",\
+authDomain:\"kgsensei-957f7.firebaseapp.com\",\
+projectId:\"kgsensei-957f7\",\
+storageBucket:\"kgsensei-957f7.appspot.com\",\
+messagingSenderId:\"816657691700\",\
+appId:\"1:816657691700:web:1f5baa75f7c716cd9a8661\",\
+measurementId:\"G-EW2T6G5DLH\"\
 };\
 firebase.initializeApp(firebaseConfig);\
 firebase.analytics();"
