ab7122a7175eafcc7010245efcdeade845b22893
KG sensei <60828719+kgsensei@users.noreply.github.com>
2021-06-07 12:58:39 -0400

    Add tracking

--- js/site.js
+++ js/site.js
@@ -1,1 +1,3 @@
+//Track visits on website for better idea of size and traffic.
+fetch("https://api.countapi.xyz/hit/kgsensei.dev/visits")
 console.log("Proudly built and published by kgsensei.\nCopyright (c) 2021 kgsensei.")
