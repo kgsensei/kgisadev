89853b1470f1d74e92a1feb039e208ff1b266ab3
KG sensei <60828719+kgsensei@users.noreply.github.com>
2021-06-09 15:01:51 -0400

    Update site.js

--- hacks/other_js/quizizz_cheats_main.js
+++ hacks/other_js/quizizz_cheats_main.js
@@ -4,13 +4,66 @@ urls=[
      "https://quizizz.com/api/main/game/60bbb4ea0251bd001d74d356"
 ]
 */
-
+let jsonData;
+function stripHTML(html){
+     let doc=new DOMParser().parseFromString(html,'text/html')
+     return doc.body.textContent||""
+}
+async function getAnswers(gamehash){
+     await fetch("https://corsbyp.herokuapp.com/https://quizizz.com/api/main/game/"+gamehash)
+          .then(response=>response.json())
+          .then(data=>jsonData=data)
+          .then(()=>console.log(jsonData))
+          console.clear()
+          if(jsonData.success==true){
+               jsonData.data.questions.forEach(questions=>{
+                    currentInt=0
+                    if(questions.type=="MCQ"){
+                         answerInt=questions.structure.answer
+                         currentQuestion=questions.structure.query.text
+                         questions.structure.options.forEach(options=>{
+                              if(answerInt==currentInt){
+                                   currentAnswer=options.text
+                                   currentInt=currentInt+1
+                              }else{
+                                   currentInt=currentInt+1
+                              }
+                         })
+                         currentQuestion=stripHTML(currentQuestion)
+                         currentAnswer=stripHTML(currentAnswer)
+                         console.log(("%cQ: "+currentQuestion+"%c - %cA: "+currentAnswer),"color:white","color:cyan","color:green")
+                    }else if(questions.type=="MSQ"){
+                         answerInt=questions.structure.answer
+                         currentQuestion=questions.structure.query.text
+                         questions.structure.options.forEach(options=>{
+                              if(answerInt==currentInt){
+                                   currentAnswer=options.text
+                                   currentInt=currentInt+1
+                              }else{
+                                   currentInt=currentInt+1
+                              }
+                         })
+                         currentQuestion=stripHTML(currentQuestion)
+                         currentAnswer=stripHTML(currentAnswer)
+                         console.log(("%cQ: "+currentQuestion+"%c - %cA: "+currentAnswer),"color:white","color:cyan","color:green")
+                    }else if(questions.type=="OPEN"){
+                         console.log(("%cCannot Solve for Open Questions."),"color:red")
+                    }else if(questions.type=="SLIDE"){
+                         console.log(("%cCannot Solve for Slide Questions."),"color:red")
+                    }else{
+                         console.log(("%cUnsupported Question Type"),"color:red")
+                    }
+               })
+          }else{
+               console.log(("%cFailed to Access Correct Answers"),"color:red")
+          }
+}
 function findAnswers() {
-     gamehash=document.getElementById("input1").value;
-     alert("Sorry buddy, this hack is still in development.\n\nIt will probably be finished around Monday (6/7/21) or Tuesday (6/8/21). Thanks for stoping by though.\n\n-kgsensei")
+     gamehash=document.getElementById("input1").value
+     getAnswers(gamehash)
 }
 
-/* Copy Paste Working Code */
+/* Copy Paste Working Code
 let vue=document.querySelector("body > div").__vue__;
 let jsonData;
 let info={
@@ -68,3 +121,4 @@ async function getAnswers(){
           })
 }
 getAnswers()
+*/
